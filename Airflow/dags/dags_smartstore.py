89ee06c287606432adf0df345aae590ab2868846
dhkim <2000daehan@naver.com>
2024-04-15 10:16:00 +0900

    smartstore add

---
+++ Airflow/dags/dags_smartstore.py
@@ -0,0 +1,36 @@
+import pendulum
+import datetime
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.email import EmailOperator
+from airflow.operators.python import PythonOperator
+from common.common_func import access_token, list_order
+
+with DAG(
+    dag_id="dags_smartstore",
+    schedule="0 8-17 * * 1-5", #매주 월-금 8-17시까지 1시간 간격으로 수행
+    start_date=pendulum.datetime(2024, 4, 15, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+
+    get_token = PythonOperator(
+        task_id = 'get_token',
+        python_callable = access_token,
+        op_kwargs = {'nav_id':'{{var.value.nav_id}}','nav_secret':'{{var.value.nav_secret}}'}
+    )
+
+    order_list = PythonOperator(
+        task_id='order_list',
+        python_callable= list_order,
+        op_kwargs={'access_token':'{{ ti.xcom_pull(task_ids="get_token") }}'}
+    )
+
+    email_alarm = EmailOperator(
+        task_id='email_alarm',
+        to='2000daehan@naver.com',
+        subject='{{ data_interval_end.in_timezone("Asia/Seoul") | ds }} 신규주문내역 {{ti.xcom_pull(task_ids="order_list")}}건',
+        html_content='작업 실행 결과는 성공입니다. <br> \
+                    주문내역 결과 {{ti.xcom_pull(task_ids="order_list")}}건입니다. <br>'
+    )
+
+order_list(get_token()) >> email_alarm
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -1,3 +1,11 @@
+import bcrypt
+import pybase64
+from datetime import datetime, timedelta, timezone
+import requests
+import json
+import time
+import pandas as pd
+
 def get_sftp():
     print('sftp작업을 실행합니다.')
 
@@ -17,3 +25,101 @@ def regist2(name, sex, *args, **kwargs):
         print(email)
     if phone:
         print(phone)
+
+def access_token(**kwargs):
+
+    id = kwargs['nav_id']
+    secret = kwargs['nav_secret']
+    dev_host = "https://api.commerce.naver.com/external"
+    #전자서명 생성
+    timestamp = int(round(time.time() * 1000))
+    password = id + "_" + str(timestamp)
+    hashed = bcrypt.hashpw(password.encode('utf-8'), secret.encode('utf-8'))
+    sign = pybase64.standard_b64encode(hashed).decode('utf-8')
+
+    #토큰 발급
+    url = f"{dev_host}/v1/oauth2/token"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    # 요청 본문 데이터
+    data = {
+        "client_id": id,
+        "timestamp": timestamp,
+        "grant_type": 'client_credentials',
+        "client_secret_sign": sign,
+        "type": 'SELF',
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+    access_token = response.json()['access_token']
+    return access_token
+
+
+def list_order(access_token):
+    # 상품 주문 내역 조회(default 24시간, 300개)
+    headers = {"Authorization" : f"Bearer {access_token}"}
+    url = "https://api.commerce.naver.com/external/v1/pay-order/seller/product-orders/last-changed-statuses"
+    now = datetime.now(timezone(timedelta(hours=9)))  # UTC+9
+    weekday = now.weekday()  # 현재 요일 (월요일: 0, 일요일: 6)
+
+    final_df = pd.DataFrame()
+
+    # 월요일이면 지난 3일간의 정보 조회, 그 외에는 최근 1일간의 정보 조회
+    if weekday == 0:
+        days_to_fetch = 3  # 월요일에는 금요일부터 일요일까지 3일간 조회
+    else:
+        days_to_fetch = 1  # 다른 날에는 당일만 조회
+
+    for days in range(days_to_fetch, 0, -1):
+        sc_from = now - timedelta(days=days)
+        
+        sc_from_str = sc_from.strftime('%Y-%m-%dT%H:%M:%S.000+09:00')
+
+        # 요청 파라미터 설정
+        params = {
+            "lastChangedFrom": sc_from_str
+        }
+
+        # GET 요청 실행
+        response = requests.get(url, headers=headers, params=params)
+
+        # 응답 검사
+        if response.status_code == 200:
+            try:
+                all_data = response.json().get('data', {}).get('lastChangeStatuses', [])
+                data_rows = []
+
+                for data in all_data:
+                    payment_date_str = data.get('paymentDate', '')
+                    payment_date = pd.to_datetime(payment_date_str) if payment_date_str else None
+
+                    # 'paymentDate'가 'sc_from' 이후인 데이터만 처리
+                    if payment_date and payment_date >= sc_from:
+                        data_row = {
+                            'productOrderId': data.get('productOrderId', ''),
+                            'orderId': data.get('orderId', ''),
+                            'productOrderStatus': data.get('productOrderStatus', ''),
+                            'paymentDate': payment_date_str,
+                            'lastChangedDate': data.get('lastChangedDate', ''),
+                            'lastChangedType': data.get('lastChangedType', ''),
+                            'receiverAddressChanged': data.get('receiverAddressChanged', ''),
+                            'claimType': data.get('claimType', ''),
+                            'claimStatus': data.get('claimStatus', ''),
+                            'giftReceivingStatus': data.get('giftReceivingStatus', '')
+                        }
+                        data_rows.append(data_row)
+
+                # 일별 데이터프레임 생성
+                day_df = pd.DataFrame(data_rows)
+                final_df = pd.concat([final_df, day_df], ignore_index=True)
+                count = len(final_df)
+
+            except Exception as e:
+                print(f"Failed to parse response from {sc_from_str}: {e}")
+        else:
+            print(f"Failed to fetch data from {sc_from_str}: {response.status_code} {response.text}")
+
+    # 최종 데이터프레임 출력
+    print(f'결제건수 : {count}건')
